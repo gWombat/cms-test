89ca9d35e71b5b8b065ad2dfd6076cc17c22cb56
gWombat <guigui.fabbi@gmail.com>
2018-04-19 16:06:28 +0200

    Initial import

---
+++ src/main/java/fr/gwombat/cmstest/custom/jackrabbit/path/CmsPathBuilder.java
@@ -0,0 +1,134 @@
+package fr.gwombat.cmstest.custom.jackrabbit.path;
+
+import java.text.MessageFormat;
+
+public final class CmsPathBuilder {
+
+    private StringBuilder pathBuilder;
+    private String        language;
+    private String        brand;
+    private String        separator;
+
+    private CmsPathBuilder(final String rootNode, final String separator) {
+        pathBuilder = new StringBuilder();
+        this.separator = separator;
+        addPath(rootNode);
+    }
+
+    public static CmsPathBuilder init(final String rootNode, final String separator) {
+        return new CmsPathBuilder(rootNode, separator);
+    }
+
+    public CmsPathBuilder addPath(final String path) {
+        if (path != null && !"".equals(path)) {
+            pathBuilder.append(path);
+            addTrailingSlash();
+        }
+        return this;
+    }
+
+    public CmsPathBuilder addPath(final String path, final Object... args) {
+        if (path != null && !"".equals(path)) {
+            final String formattedPath = MessageFormat.format(path, args);
+            return addPath(formattedPath);
+        }
+        return this;
+    }
+
+    public CmsPathBuilder addProperty(final String propertyName) {
+        if (propertyName != null && !"".equals(propertyName))
+            pathBuilder.append(propertyName);
+        return this;
+    }
+
+    public CmsPathBuilder language(final String language) {
+        if (language != null && !"".equals(language))
+            this.language = language;
+        return this;
+    }
+
+    public CmsPathBuilder brand(final String brand) {
+        if (brand != null && !"".equals(brand))
+            this.brand = brand;
+        return this;
+    }
+
+    public CmsPathBuilder withCityIdIfNecessary(boolean isNecessary, Long cityId) {
+        if (isNecessary)
+            return withCityId(cityId, "");
+        return this;
+    }
+
+    public CmsPathBuilder withCityId(final Long cityId, final String separator) {
+        if (cityId != null) {
+            removeTrailingSlash();
+            pathBuilder.append(separator);
+            pathBuilder.append(String.valueOf(cityId));
+            addTrailingSlash();
+        }
+        return this;
+    }
+
+    public JackrabbitPath build() {
+        return new JackrabbitPath(this);
+    }
+
+    String buildCmsPath() {
+        final String path = validateAndGetPath();
+        if (path.isEmpty())
+            return null;
+        return path;
+    }
+
+    String buildFullPath() {
+        final String prefix = buildPrefix();
+        return prefix + buildCmsPath();
+    }
+
+    private String buildPrefix() {
+        final StringBuilder prefix = new StringBuilder();
+        if (language != null && !"".equals(language))
+            prefix.append(language).append(separator);
+        if (brand != null && !"".equals(brand))
+            prefix.append(brand).append(separator);
+        return prefix.toString();
+    }
+
+    private String validateAndGetPath() {
+        String path = pathBuilder.toString();
+        if (!"".equals(path)) {
+            path = path.trim();
+            removeLeadingSlash(pathBuilder);
+            removeTrailingSlash();
+        }
+        return path;
+    }
+
+    private void removeLeadingSlash(final StringBuilder stringBuilder) {
+        if (stringBuilder.toString().startsWith(separator))
+            stringBuilder.deleteCharAt(0);
+    }
+
+    private void addTrailingSlash() {
+        if (!pathBuilder.toString().endsWith(separator))
+            pathBuilder.append(separator);
+    }
+
+    private void removeTrailingSlash() {
+        if (pathBuilder.toString().endsWith(separator))
+            pathBuilder.deleteCharAt(pathBuilder.length() - 1);
+    }
+
+    @Override
+    public String toString() {
+        return buildFullPath();
+    }
+
+    String getLanguage() {
+        return language;
+    }
+
+    String getBrand() {
+        return brand;
+    }
+}
---
+++ src/main/java/fr/gwombat/cmstest/custom/jackrabbit/path/JackrabbitPath.java
@@ -0,0 +1,58 @@
+package fr.gwombat.cmstest.custom.jackrabbit.path;
+
+import fr.gwombat.cmstest.core.path.CmsPath;
+
+import java.util.Objects;
+
+public class JackrabbitPath implements CmsPath {
+
+    private final String language;
+    private final String brand;
+    private final String path;
+    private final String fullCmsPath;
+
+    JackrabbitPath(final CmsPathBuilder builder) {
+        this.path = builder.buildCmsPath();
+        this.fullCmsPath = builder.buildFullPath();
+        this.brand = builder.getBrand();
+        this.language = builder.getLanguage();
+    }
+
+    @Override
+    public String toString() {
+        return getFullCmsPath();
+    }
+
+    /**
+     * @return the fullCmsPath
+     */
+    public String getFullCmsPath() {
+        return fullCmsPath;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JackrabbitPath jackrabbitPath = (JackrabbitPath) o;
+        return Objects.equals(fullCmsPath, jackrabbitPath.fullCmsPath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullCmsPath);
+    }
+}
+
